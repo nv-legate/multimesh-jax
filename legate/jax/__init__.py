bbeaab27fdec678769e69da606e22b8a855b7524
jjwilke <jwilke@nvidia.com>
2023-10-04 15:29:20 -0700

    add microbatch custom call

--- legate/jax/__init__.py
+++ legate/jax/__init__.py
@@ -1,3 +1,4 @@
-from .legate_jax_impl import shutdown
+from .legate_jax_impl import shutdown, no_op_custom_call
 from .lib import init
 from .task import task
+from .no_op import microbatch
--- src/plugin/legate_jax_impl.cc
+++ src/plugin/legate_jax_impl.cc
@@ -5,9 +5,37 @@ void StopLegate();
 }
 extern "C" void ShutdownLegateClient();
 
+template <class To, class From>
+typename std::enable_if<sizeof(To) == sizeof(From) &&
+                            std::is_trivially_copyable<From>::value &&
+                            std::is_trivially_copyable<To>::value,
+                        To>::type
+bit_cast(const From &src) noexcept {
+  static_assert(std::is_trivially_constructible<To>::value,
+                "This implementation additionally requires destination type to "
+                "be trivially constructible");
+
+  To dst;
+  memcpy(&dst, &src, sizeof(To));
+  return dst;
+}
+
+template <typename T> pybind11::bytes PackDescriptor(const T &descriptor) {
+  return pybind11::bytes(PackDescriptorAsString(descriptor));
+}
+
+template <typename T> pybind11::capsule EncapsulateFunction(T *fn) {
+  return pybind11::capsule(bit_cast<void *>(fn), "xla._CUSTOM_CALL_TARGET");
+}
+
+void no_op_entrypoint(){}
+
 PYBIND11_MODULE(legate_jax_impl, m) {
   m.def("shutdown", []() -> void {
     legate_xla::StopLegate();
     ShutdownLegateClient();
+  });
+  m.def("no_op_custom_call", [](){
+    return EncapsulateFunction(no_op_entrypoint);
   });
 }
