761a401055f68bde66ded6a307348547e2f15d50
jjwilke <jwilke@nvidia.com>
2023-11-05 19:11:33 -0800

    Updated dockerfile and clone scripts

--- docker/clone-all.sh
+++ docker/clone-all.sh
@@ -2,6 +2,8 @@
 
 git clone -b control_replication ssh://git@gitlab.com/StanfordLegion/legion.git legion
 git clone -b cpp-branch-23.09 ssh://git@github.com/nv-legate/legate.core.internal legate
-git clone --filter tree:0 ssh://git@gitlab-master.nvidia.com:12051/legate/xla.git xla_extension
+git clone -b legate-main --filter tree:0 git@github.com:nv-legate/xla.git xla_extension
 git clone ssh://git@gitlab-master.nvidia.com:12051/legate/legate-xla-bridge.git legate_xla
+git clone git@github.com:nv-legate/jax.git jaxlib
+git clone git@github.com:nv-legate/jax.git jax
 
