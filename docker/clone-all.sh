d7cda83eca94925d1ed7e2745dc156f4082e95c5
Jeremy <jjwilke@users.noreply.github.com>
2024-01-09 07:09:44 -0800

    Docker build fixes (#11)

--- docker/clone-all.sh
+++ docker/clone-all.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
 git clone -b control_replication ssh://git@gitlab.com/StanfordLegion/legion.git legion
-git clone -b xla-bridge ssh://git@github.com/jjwilke/legate.core.internal legate
+git clone -b cpp-branch-24.01 ssh://git@github.com/nv-legate/legate.core.internal legate
 git clone -b legate-main --filter tree:0 git@github.com:nv-legate/xla.git xla_extension
 git clone ssh://git@github.com/nv-legate/legate.jax.git legate_xla
 git clone -b legate-main git@github.com:nv-legate/jax.git jaxlib
