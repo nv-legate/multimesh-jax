d3212db4b4e18851a3299d28751a1e447996d8f2
Jeremy <jjwilke@users.noreply.github.com>
2024-06-12 11:56:40 -0700

    Update shas before june rebase (#110)
    * update shas and other build fixes

--- docker/clone-all.sh
+++ docker/clone-all.sh
@@ -6,12 +6,17 @@ function checkout {
   repo=$3
   commit=$4
   patch=$5
-  if [[ -d "$folder" && -z "$commit" ]]; then
-    echo "updating $branch of $repo to top-of-tree for $folder"
+  if [[ -d "$folder" ]]; then
     pushd $folder
+    echo "fetching updates for $folder"
     git fetch origin
-    git checkout origin/$branch
-    popd
+    if [[ -z "$commit" ]]; then
+      echo "updating $branch of $repo to top-of-tree for $folder"
+      git checkout origin/$branch
+    else
+      echo "checking out commit $commit in $folder"
+      git checkout origin/$branch
+    fi
   else
     echo "cloning $branch of $repo into $folder"
     git clone -b $branch --filter tree:0 $repo $folder
@@ -22,27 +27,26 @@ function checkout {
     if [ ! -z "$patch" ]; then
       git apply ../$patch
     fi
-    popd
   fi
+  popd
 }
 
-checkout legion     master              ssh://git@gitlab.com/StanfordLegion/legion.git      2f87f39e91b8c95de816d207ff03fcada152664f
-checkout legate     cpp-branch-24.05    ssh://git@github.com/nv-legate/legate.core.internal 5a650f68fbd42c50a7b3d323e8b87f183f733205 legate.patch
-checkout xla        legate-main         ssh://git@github.com/nv-legate/xla.git              d7bebc46549cfb8553629f41cd90d2c830e0695f
-checkout legate-jax main                ssh://git@github.com/nv-legate/legate.jax.git       102c3342f9c3ba49af908ffc763c2a027d2da47c
-checkout jaxlib     legate-main         ssh://git@github.com/nv-legate/jax.git              67afa4c032cdeac7e9d72ba210bd851d2d470bfb
-checkout jax        legate-main         ssh://git@github.com/nv-legate/jax.git              67afa4c032cdeac7e9d72ba210bd851d2d470bfb
-checkout paxml      main                https://github.com/google/paxml.git                 bd0590f843 paxml.patch
-checkout praxis     main                https://github.com/google/praxis.git                c58bcc4e82 praxis.patch
-checkout orbax      main                https://github.com/google/orbax.git                 6673d0c
-checkout clu        main                https://github.com/google/CommonLoopUtils.git       c50acb7
-checkout optax      main                https://github.com/google-deepmind/optax.git        b4acf8e
-checkout flax       main                https://github.com/google/flax.git                  fdbc640c   flax.patch
-checkout seqio      main                https://github.com/google/seqio.git                 11706e4a1e
-checkout chex       master              https://github.com/google-deepmind/chex.git         53ba23c51
+checkout chex       master              https://github.com/google-deepmind/chex.git   a3f1dd0
+checkout clu        main                https://github.com/google/CommonLoopUtils.git f30bc44
+checkout flax       main                https://github.com/google/flax.git            fdbc640cfc8 flax.patch
+checkout jax        legate-main         ssh://git@github.com/nv-legate/jax.git        67afa4c0
+checkout jaxlib     legate-main         ssh://git@github.com/nv-legate/jax.git        67afa4c0
+checkout legate     cpp-branch-24.05    ssh://git@github.com/nv-legate/legate.core.internal 1c5e17e1f legate.patch
+checkout legate-jax main                ssh://git@github.com/nv-legate/legate.jax.git 78eba668171
+checkout legion     master              ssh://git@gitlab.com/StanfordLegion/legion.git ae7bd61186
+checkout optax      main                https://github.com/google-deepmind/optax.git  a49564e
+checkout orbax      main                https://github.com/google/orbax.git           6673d0c
+checkout paxml      main                https://github.com/google/paxml.git           bd0590f843   paxml.patch
+checkout praxis     main                https://github.com/google/praxis.git          c58bcc       praxis.patch
+checkout seqio      main                https://github.com/google/seqio.git           513d1fe
 checkout te         main                https://github.com/NVIDIA/TransformerEngine.git     1ec33ae11  te.patch
+checkout xla        legate-main         ssh://git@github.com/nv-legate/xla.git        e9723f5b4902
 
 pushd te
 git submodule update --init --recursive
 popd
-
